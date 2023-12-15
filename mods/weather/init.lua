20bc47834ec31262e6c2bfaa71aa782a079780b1
Talon Kettuso <105325988+TalonFox@users.noreply.github.com>
2023-12-14 20:55:23 -0600

    raa
    Signed-off-by: Talon Kettuso <105325988+TalonFox@users.noreply.github.com>

--- mods/weather/init.lua
+++ mods/weather/init.lua
@@ -1,7 +1,13 @@
 weather = {}
 
 weather.active_condition = nil
+weather.time_left = 0
 weather.events = {}
+
+minetest.register_globalstep(function(t)
+    weather.time_left = weather.time_left - t
+    
+end)
 
 function weather.register_event(event)
     
